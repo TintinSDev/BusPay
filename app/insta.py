913f11182bf70dc9db57dc78cbc7ae72d533cbd8
Martin <mmaina290@gmail.com>
2024-05-15 14:01:24 +0300

    payments modified

--- app/insta.py
+++ app/insta.py
@@ -10,7 +10,7 @@ service = APIService(token=token, publishable_key=publishable_key, test=True)
 token = "YOUR-API-TOKEN"
 service = APIService(token="token")
 
-response = service.chargebacks.create(invoice="<invoice-id>", amount=1, reason = "fraud", reason_details = "fraud")
+response = service.chargebacks.create(invoice="<invoice-id>", amount=50, reason = "fraud", reason_details = "fraud")
 print(response)
 
 publishable_key = "ISPubKey_test_73158406-58db-43d2-8b6d-4e1c2b391929"
@@ -18,7 +18,7 @@ publishable_key = "ISPubKey_test_73158406-58db-43d2-8b6d-4e1c2b391929"
 service = APIService(publishable_key=publishable_key)
 
 response = service.collect.mpesa_stk_push(phone_number=2547.,
-                                  email="joe@doe.com", amount=1, narrative="Purchase")
+                                  email="joe@doe.com", amount=15, narrative="Purchase")
 
 print(response)
 
--- src/client/Inta.jsx
+++ src/client/Inta.jsx
@@ -1,28 +1,102 @@
+import { useState } from 'react';
 import 'intasend-inlinejs-sdk'
-import '../inta.css'
 
-function MyScreen(){
+function MyScreen() {
   
-  new window.IntaSend({
-    publicAPIKey: "ISPubKey_test_73158406-58db-43d2-8b6d-4e1c2b391929",
-    live: false //or true for live environment
-  })
-  .on("COMPLETE", (response) => { console.log("COMPLETE:", response) })
-  .on("FAILED", (response) => { console.log("FAILED", response) })
-  .on("IN-PROGRESS", () => { console.log("INPROGRESS ...") })
+  const [paymentMethod, setPaymentMethod] = useState('');
+  const [amount, setAmount] = useState('');
 
-  return(
-    <div>
-      <input type="text" placeholder="Amount"  />
-            <input type="text" placeholder="Phone Number" />
-        <button className="intaSendPayButton" data-amount="10" data-currency="KES">Pay via M-PESA</button>
-      
+  const handlePaymentMethodChange = (e) => {
+    setPaymentMethod(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handlePayNowClick = () => {
+      new window.IntaSend({
+        publicAPIKey: "ISPubKey_test_73158406-58db-43d2-8b6d-4e1c2b391929",
+        live: false, // or true for live environment
+      })
+        .on("COMPLETE", (response) => {
+          console.log("COMPLETE:", response);
+          // Handle successful payment
+        })
+        .on("FAILED", (response) => {
+          console.log("FAILED", response);
+          // Handle failed payment
+          alert("Payment failed. Please try again later.");
+        })
+        .on("IN-PROGRESS", () => {
+          console.log("INPROGRESS ...");
+          // Handle in-progress payment
+        })
+        .open({
+          amount: parseFloat(amount),
+          currency: "KES",
+          customer: {
+            name: "John Doe",
+            email: "johndoe@example.com",
+            phone: "0712345678",
+          },
+        });
+    };
+
+  const handlePayViaCashClick = () => {
+    console.log("Pay via cash");
+  };
+
+  return (
+    <div className="container">
+      <div className="payment-options">
+        <h2>Payment Options</h2>
+        <label>
+          <input
+            type="radio"
+            name="payment-method"
+            value="cash"
+            checked={paymentMethod === 'cash'}
+            onChange={handlePaymentMethodChange}
+          />
+          Cash
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="payment-method"
+            value="intasend"
+            checked={paymentMethod === 'intasend'}
+            onChange={handlePaymentMethodChange}
+          />
+          Intasend
+        </label>
+        <div>
+          <label htmlFor="amount">Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={handleAmountChange}
+            required
+          />
+        </div>
+        {paymentMethod === 'intasend' && (
+          <div>
+            <button className="intaSendPayButton" data-amount= {amount} data-currency="KES" onClick={handlePayNowClick}>
+              Pay Now
+            </button>
+          </div>
+        )}
+        {paymentMethod === 'cash' && (
+          <div>
+            <button onClick={handlePayViaCashClick}>Pay via Cash</button>
+          </div>
+        )}
+      </div>
     </div>
-  )
-  
-  
+  );
 }
-
 
 export default MyScreen
 
